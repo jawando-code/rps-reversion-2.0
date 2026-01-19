8a8c4d4768c2d62a13aa695c3d43ac56cc476e98
jawando-code <peruvianswing@gmail.com>
2026-01-19 14:22:44 +0000

    Finish Internal Logic
    Design and iterate JS Dom code to change values;
    Add moving background element from online codepen.
    Add pictures to UI.
    Add external font

--- rps.js
+++ rps.js
@@ -1,21 +1,27 @@
 const MAXROUNDS = 5;
-let rounds = 0;
+let rounds = 1;
 let computerScore = 0;
 let humanScore = 0;
 
 
-function playGame() {
-    
-    if(computerScore > humanScore){
-        console.log("COMPUTER WINS THIS ROUND! DOOOM")
-    } else if(humanScore > computerScore){
-        console.log("YOU WIN THIS ROUND")
-    } else {
-        console.log("DRAW! play again?")
-    }
-}
+const buttons = document.querySelectorAll('.buttons')
+const roundDisp = document.querySelector(".rounds")
+const roundPara = document.createElement('p');
+const displayHuman = document.querySelector('.display2')
+const displayCom = document.querySelector('.display')
+const cr8Para = document.createElement('p')
+const cr8Para2 = document.createElement('p')
+const message = document.querySelector('.message')
+const start = document.querySelector('.start');
+const stats = document.querySelector('.stats')
+const computerScoreDis = document.querySelector('.computerScore');
+const humanScoreDis = document.querySelector('.humanScore');
+    const display = document.querySelector('.display');
+
+
 
 let computerChoice ;
+let humanChoice ;
 function getComputer() {
         computerChoice = '';
         let weapons = ["rock","paper","scissors"];
@@ -25,23 +31,39 @@ function getComputer() {
         return computerChoice;
 }
 
-let humanChoice ;
+
+humanScoreDis.textContent = `You: ${humanScore}`
+stats.appendChild(humanScoreDis)
+
+
+computerScoreDis.textContent = `Computer: ${computerScore}`;
+stats.appendChild(computerScoreDis);
+
+
 
 
 function playround() {
+
+
 if (computerChoice === humanChoice)
     {
-        console.log("Draw! Play again")
+        console.log("Draw! Play again");
+        display.textContent = "Draw! Play again.";
         return "Draw";
+
     } else if (
         (computerChoice === "rock" && humanChoice === "scissors" ) ||
         (computerChoice === "paper" && humanChoice === "rock") ||
         (computerChoice === "scissors" && humanChoice === "paper"
         )
         )
-{
-    console.log("Computer Wins");
-    ++computerScore;
+{ 
+        ++computerScore;
+        console.log("Computer Wins");
+        computerScoreDis.textContent = `Computer: ${computerScore}`;
+        
+stats.appendChild(computerScoreDis);
+    display.textContent =`Computer Wins! ${computerChoice} beats ${humanChoice}!`;
     return  "Computer Wins! You loose.";
 } else if (
     (humanChoice === "rock" && computerChoice === "scissors") ||
@@ -49,38 +71,141 @@ if (computerChoice === humanChoice)
     (humanChoice === "scissors" & computerChoice === "paper")
     )
 {
-    console.log("You Win");
-    ++humanScore;
-return "You win! Computer looses.";
+     ++humanScore; 
+    humanScoreDis.textContent = `You: ${humanScore}`
+    stats.appendChild(humanScoreDis);
+   display.textContent = `You win! ${humanChoice} beats ${computerChoice}!`;
+   
 }
 
+
+
+humanScoreDis.textContent = `You: ${humanScore}`
+stats.appendChild(humanScoreDis)
+}
+
+function fiveRounds() {
+    
+    humanScoreDis.textContent = `You: ${humanScore}`
+    stats.appendChild(humanScoreDis);
+    computerScoreDis.textContent = `Computer: ${computerScore}`;
+    stats.appendChild(computerScoreDis); 
+    newround.style.display = 'none';
+    start.style.display = 'block';
+    
+      
+    }
+
+    // humanScore = 0;
+    // computerScore = 0;
+    // rounds = 0;
+    
+
+function playGame() {
+
+    if(rounds === MAXROUNDS) {
+        if(computerScore > humanScore) { 
+            console.log("COMPUTER WINS THIS ROUND! DOOOM")
+            message.textContent ="COMPUTER WINS THIS ROUND! DOOOM";
+        } else if(humanScore > computerScore){
+            console.log("YOU WIN THIS ROUND")
+            message.textContent ="YOU WIN THIS ROUND"
+        } else { 
+            console.log("DRAW! play again?")
+            message.textContent ="DRAW! Play again?"
+        }
+       fiveRounds(); 
+    }
+    
 }
 
 
+function disableButtons() {
+        buttons.forEach(button => {
+            button.disabled = true;
+           
+        })
+}
+
+function enableButtons() {
+        buttons.forEach(button => {
+            button.disabled = false;
+        })
+}
+
+
+roundPara.textContent = `Round: ${rounds}`;
+roundDisp.appendChild(roundPara);
+const newround = document.getElementById('newround');
+
+newround.addEventListener('click', () => {
+     rounds++;
+    newround.style.backgroundColor = 'grey';
+    buttons.forEach((button) => {
+        button.style.backgroundColor = '#4A0505'
+    })
+    })
+ 
+newround.addEventListener('click', (event) => {
+    enableButtons() })
 
 
 
-const buttons = document.querySelectorAll('.buttons')
 
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
-    humanChoice = button.value; 
+    humanChoice = button.value;
     console.log(humanChoice)   
     return humanChoice;   
 })
 
+
+
+
+
 button.addEventListener("click", getComputer)
 button.addEventListener("click",playround);
+button.addEventListener("click",playGame);
+
+
 button.addEventListener('click', () => {
-    button.style.backgroundColor = 'red'
+    button.style.backgroundColor = 'red';
+
+})
+button.addEventListener('click', () => {
+   
+    roundPara.textContent = `Round: ${rounds}`;
+    roundDisp.appendChild(roundPara);
+})
+
+button.addEventListener('click', (event) => {    
+    disableButtons();
+} )
+
+    
+    
 })
 
 
+
+
+
+start.addEventListener('click', () => {
+    enableButtons();
+    buttons.forEach((button) => {
+        button.style.backgroundColor = '#4A0505'
+    })
+    humanScore = 0;
+    humanScoreDis.textContent = `You: ${humanScore}`;
+    
+    computerScore = 0;
+    computerScoreDis.textContent = `Computer: ${computerScore}`;
+    rounds = 1;
+    roundPara.textContent = `Round: ${rounds}`
+
+    message.textContent = '';
+    display.textContent = '';
+    newround.style.display = 'block';
+    
+    start.style.display = 'none';
 })
-
-// function getHuman(str) {
-//     str = prompt("Choose your weapon",'')
-//     console.log(str)
-//     return str;} 
-
-
